77ada01cee9a5179c0add4bfac252f3f079a3615
Rohit <rohit.karma026@gmail.com>
2024-03-13 07:53:13 +0530

    final commit

--- src/todoapp/LogoutComponent.js
+++ src/todoapp/LogoutComponent.js
@@ -5,7 +5,7 @@
       <div className="logoutComponent text-center py-5">
       <h1 className="mb-4">You are logged out</h1>
       <p className="mb-4">Thank you for using our app.</p>
-      <p>Redirecting you to the login page...</p>
+      <p>Redirecting you to the login page... <a href="/login">click here</a> </p>
   </div>
   
     )
